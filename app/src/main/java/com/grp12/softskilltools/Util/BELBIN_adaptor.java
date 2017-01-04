9def60cb98ec4975d09772934ee0a8c4a8cebf72
Arbab Javaid <arbabjava@gmail.com>
2017-01-04 12:46:03 +0100

    Begyndelse af Belbin_adaptor

---
+++ app/src/main/java/com/grp12/softskilltools/Util/BELBIN_adaptor.java
@@ -0,0 +1,50 @@
+package com.grp12.softskilltools.Util;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.grp12.softskilltools.Entities.Question;
+
+/**
+ * Created by IbnMichael on 04/01/17.
+ */
+
+public class Belbin_adaptor {
+
+
+    public class Belbin_adaptor extends BaseAdapter {
+
+        private static ArrayList<Question> questions;
+        private LayoutInflater mInflater;
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+
+
+    }
+
+    static class ViewHolder{
+        TextView questions;
+    }
+
+
+}
