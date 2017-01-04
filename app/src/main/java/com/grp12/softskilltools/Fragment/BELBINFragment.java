bc4d95eb15332d74fc241ff0cdaa94b979eb6016
rosenlund96 <Stumack3r>
2017-01-04 11:52:56 +0100

    Igang med Belbin

--- app/src/main/java/com/grp12/softskilltools/Fragment/BELBINFragment.java
+++ app/src/main/java/com/grp12/softskilltools/Fragment/BELBINFragment.java
@@ -1,9 +1,26 @@
 package com.grp12.softskilltools.Fragment;
 
+import android.support.v4.app.Fragment;
+import android.view.View;
+
 /**
  * Created by Mathias R. Larsen on 03-01-2017.
  */
 
-public class BELBINFragment {
+public class BELBINFragment extends Fragment implements View.OnClickListener {
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
 
