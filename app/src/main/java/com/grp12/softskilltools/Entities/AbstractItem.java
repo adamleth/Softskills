b65ba4ae977f5809796efac9ec2b2844d7ba1a16
Arbab Javaid <arbabjava@gmail.com>
2017-01-04 14:53:56 +0100

    JavaDoczzzzz - AbstractItemEnti

--- app/src/main/java/com/grp12/softskilltools/Entities/AbstractItem.java
+++ app/src/main/java/com/grp12/softskilltools/Entities/AbstractItem.java
@@ -25,21 +25,86 @@ public abstract class AbstractItem implements Serializable {
 
     }
     public testType getTestType(){
+
+        /***********************************************
+         * This method checks for the type of the test *
+         ***********************************************/
+
         return type;
     }
-    public void setTestType(testType type) {this.type = type;}
+    public void setTestType(testType type) {
+
+        /*****************************************
+         * This method sets the type of the test *
+         *****************************************/
+
+        this.type = type;}
+
+
     public User getOwner(){
+
+        /******************************
+         * This method gets the owner *
+         ******************************/
+
         return owner;
     }
     public void setOwner(User owner) {
+
+        /****************************************
+         * This method sets the owner of a test *
+         ****************************************/
+
         this.owner = owner;
     }
-    public void setName(String name) { this.productName = name;}
-    public void setCost(int cost) { this.cost = cost;}
-    public String getProductName(){ return productName;}
-    public String getDescription(){ return description;}
-    public String getPrice() {return Double.toString(cost);}
-    public int getCompletion(){ return 1;}
+    public void setName(String name) {
+
+        /******************************************
+         * This method sets the name of the owner *
+         ******************************************/
+
+        this.productName = name;}
+
+    public void setCost(int cost) {
+
+        /****************************************
+         * This method sets the price of a test *
+         ****************************************/
+
+        this.cost = cost;}
+
+    public String getProductName(){
+
+        /***************************************
+         * This method gets the name of a test *
+         ***************************************/
+
+        return productName;}
+
+    public String getDescription(){
+
+        /**********************************************
+         * This method gets the description of a test *
+         **********************************************/
+
+        return description;}
+
+    public String getPrice() {
+
+        /****************************************
+         * This method gets the price of a test *
+         ****************************************/
+
+        return Double.toString(cost);}
+
+    public int getCompletion(){
+
+        /*******************************************************************
+         * This method gets the information regarding completion of a test *
+         *******************************************************************/
+
+        return 1;}
+
 
     @Override
     public String toString(){
