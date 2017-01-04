9fda99cca561b26f7ea83be2e0258e8e4c1e044f
rosenlund96 <Stumack3r>
2017-01-04 12:59:54 +0100

    Opdateringer til grafer

--- app/src/main/java/com/grp12/softskilltools/Fragment/DISCResultFragment.java
+++ app/src/main/java/com/grp12/softskilltools/Fragment/DISCResultFragment.java
@@ -1,5 +1,6 @@
 package com.grp12.softskilltools.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,12 +8,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.galgespil.stvhendeop.menuapp.R;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
+
 import com.grp12.softskilltools.Entities.AbstractItem;
 import com.grp12.softskilltools.Entities.DISC;
+
+import org.eazegraph.lib.communication.IOnItemFocusChangedListener;
+import org.eazegraph.lib.models.PieModel;
+import org.eazegraph.lib.charts.PieChart;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,42 +27,39 @@ import static com.galgespil.stvhendeop.menuapp.R.id.pieChart;
 
 public class DISCResultFragment extends Fragment {
 
-    PieChart result;
+    private PieChart mPieChart;
     DISC temp;
     Bundle bundle;
-    View myView;
 
+    public DISCResultFragment(){
+
+    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        myView = inflater.inflate(R.layout.frag_disc_result, container, false);
-        result = (PieChart) myView.findViewById(pieChart);
+        View view = inflater.inflate(R.layout.frag_disc_result, container, false);
+        mPieChart = (PieChart) view.findViewById(R.id.pieChart);
+
         bundle = getArguments();
         this.temp =(DISC) bundle.getSerializable("Item");
-        data(temp.getDom(),temp.getInf(),temp.getSta(),temp.getCom());
+        loadData(temp.getDom(),temp.getInf(),temp.getSta(),temp.getCom());
         //Testresultater skap parses direkte fra listresultfragment, via en putextra funktion. På denne måde sendes data direkte til dette framgent, og loader med det samme.
-        return myView;
+        return view;
     }
 
-    public void data(float f1, float f2, float f3, float f4) {
 
+    private void loadData(int dom, int inf,int sta,int com) {
+        mPieChart.addPieSlice(new PieModel("Freetime", dom, Color.parseColor("#FE6DA8")));
+        mPieChart.addPieSlice(new PieModel("Sleep", inf, Color.parseColor("#56B7F1")));
+        mPieChart.addPieSlice(new PieModel("Work", sta, Color.parseColor("#CDA67F")));
+        mPieChart.addPieSlice(new PieModel("Eating", com, Color.parseColor("#FED70E")));
 
-
-
-
-        List<PieEntry> entries = new ArrayList<>();
-
-        entries.add(new PieEntry(f1, "Dom"));
-        entries.add(new PieEntry(f2, "Inf"));
-        entries.add(new PieEntry(f3, "Sta"));
-        entries.add(new PieEntry(f4, "Com"));
-
-
-        PieDataSet set = new PieDataSet(entries, "DISC profil resultat");
-        PieData data = new PieData(set);
-        result.setData(data);
-        result.invalidate();
-
+        mPieChart.setOnItemFocusChangedListener(new IOnItemFocusChangedListener() {
+            @Override
+            public void onItemFocusChanged(int _Position) {
+//                Log.d("PieChart", "Position: " + _Position);
+            }
+        });
     }
 }
