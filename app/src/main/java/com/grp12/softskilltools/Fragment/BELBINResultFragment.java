17a918f5624cfeb2391792919957e213fff22bd3
rosenlund96 <Stumack3r>
2017-01-06 15:55:01 +0100

    Belbinresultat burde nu virke

--- app/src/main/java/com/grp12/softskilltools/Fragment/BELBINResultFragment.java
+++ app/src/main/java/com/grp12/softskilltools/Fragment/BELBINResultFragment.java
@@ -1,6 +1,7 @@
 package com.grp12.softskilltools.Fragment;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -11,9 +12,11 @@ import com.galgespil.stvhendeop.menuapp.R;
 import com.github.mikephil.charting.charts.RadarChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
 import com.grp12.softskilltools.Entities.BELBIN;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Mathias R. Larsen on 03-01-2017.
@@ -23,7 +26,7 @@ public class BELBINResultFragment extends Fragment {
 
     BELBIN temp;
     RadarChart spider;
-    ArrayList<Entry> data;
+    List<RadarEntry> data;
     Bundle bundle;
 
 
@@ -36,7 +39,7 @@ public class BELBINResultFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_belbin_result, container, false);
         spider = (RadarChart) view.findViewById(R.id.spiderweb);
-        data = new List<Entry>();
+        data = new ArrayList<>();
         bundle = getArguments();
         this.temp = (BELBIN) bundle.getSerializable("Item");
         loadData(temp.getPL(),temp.getRI(),temp.getCO(),temp.getSH(),temp.getME(),temp.getTW(),temp.getIMP(),temp.getCF(),temp.getSP(),temp.getDROP());
@@ -44,20 +47,21 @@ public class BELBINResultFragment extends Fragment {
     }
 
     public void initialize () {
-        RadarDataSet datasetPL = new RadarDataSet(data,"Dine data");
+        RadarDataSet dataset = new RadarDataSet(data,"Dine data");
+        dataset.setColor(Color.parseColor("#e94848"));
     }
 
     private void loadData(int PL, int RI, int CO, int SH, int ME, int TW, int IMP, int CF, int SP, int DROP) {
-        data.add(new Entry(PL,0));
-        data.add(new Entry(RI,1));
-        data.add(new Entry(CO,2));
-        data.add(new Entry(SH,3));
-        data.add(new Entry(ME,4));
-        data.add(new Entry(TW,5));
-        data.add(new Entry(IMP,6));
-        data.add(new Entry(CF,7));
-        data.add(new Entry(SP,8));
-        data.add(new Entry(DROP,9));
+        data.add(new RadarEntry(PL,0));
+        data.add(new RadarEntry(RI,1));
+        data.add(new RadarEntry(CO,2));
+        data.add(new RadarEntry(SH,3));
+        data.add(new RadarEntry(ME,4));
+        data.add(new RadarEntry(TW,5));
+        data.add(new RadarEntry(IMP,6));
+        data.add(new RadarEntry(CF,7));
+        data.add(new RadarEntry(SP,8));
+        data.add(new RadarEntry(DROP,9));
 
     }
 
