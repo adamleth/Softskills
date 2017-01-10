4c7c416565495849c6487920893245b4c6abb45d
sebastianhansen <sebastian.p.hansen@gmail.com>
2017-01-10 10:56:18 +0100

    Endnu en layout fil til ViewPager tilføjet

--- app/src/main/java/com/grp12/softskilltools/Fragment/ScreenSlidePageFragment.java
+++ app/src/main/java/com/grp12/softskilltools/Fragment/ScreenSlidePageFragment.java
@@ -13,8 +13,7 @@ import com.galgespil.stvhendeop.menuapp.R;
 
 public class ScreenSlidePageFragment extends Fragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_screen_slide_page, container, false);
 
