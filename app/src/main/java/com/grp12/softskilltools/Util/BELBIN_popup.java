b53b6b201f18640460f5283cf680ad72c6c18c1d
Adam <s153850@student.dk>
2017-01-17 12:56:16 +0100

    lavet popup på belbin

--- app/src/main/java/com/grp12/softskilltools/Util/BELBIN_popup.java
+++ app/src/main/java/com/grp12/softskilltools/Util/BELBIN_popup.java
@@ -15,20 +15,20 @@ import com.galgespil.stvhendeop.menuapp.R;
  * Created by Ejer on 16-01-2017.
  */
 
-public class DISC_popup extends Activity implements View.OnClickListener {
+public class BELBIN_popup extends Activity implements View.OnClickListener {
 
-    TextView discPopTitle, discPopText;
-    Button discPopOK;
+    TextView belbinPopTitle, belbinPopText;
+    Button belbinPopOK;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_popup);
-        discPopTitle = (TextView) findViewById(R.id.popTitle);
-        discPopText = (TextView) findViewById(R.id.popText);
-        discPopOK = (Button) findViewById(R.id.popOK);
-        discPopOK.setOnClickListener(this);
+        belbinPopTitle = (TextView) findViewById(R.id.popTitle);
+        belbinPopText = (TextView) findViewById(R.id.popText);
+        belbinPopOK = (Button) findViewById(R.id.popOK);
+        belbinPopOK.setOnClickListener(this);
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
         int width = dm.widthPixels;
@@ -37,19 +37,19 @@ public class DISC_popup extends Activity implements View.OnClickListener {
         final int antal = PromptIntent.getIntExtra("antal",1);
         final String tempBrødtekst = PromptIntent.getStringExtra("brødtekst");
         final String tempOverskrift = PromptIntent.getStringExtra("overskrift");
-        setDiscTekst(tempBrødtekst, tempOverskrift);
+        setBelbinTekst(tempBrødtekst, tempOverskrift);
         System.out.println(antal);
         getWindow().setLayout((int) (width * .9), (int) (height * .4));
     }
 
-    public void setDiscTekst(String overskrift, String brødtekst) {
-        discPopText.setText(brødtekst);
-        discPopTitle.setText(overskrift);
+    public void setBelbinTekst(String overskrift, String brødtekst) {
+        belbinPopText.setText(brødtekst);
+        belbinPopTitle.setText(overskrift);
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == discPopOK.getId()) {
+        if (v.getId() == belbinPopOK.getId()) {
             finish();
         }
     }
