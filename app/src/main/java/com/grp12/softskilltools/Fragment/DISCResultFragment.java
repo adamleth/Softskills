a7329c49320a75e5d41beb42a098ea9ce67dd7e3
Adam <s153850@student.dk>
2017-01-17 09:59:49 +0100

    lavet popup på disc-resultat

--- app/src/main/java/com/grp12/softskilltools/Fragment/DISCResultFragment.java
+++ app/src/main/java/com/grp12/softskilltools/Fragment/DISCResultFragment.java
@@ -1,5 +1,6 @@
 package com.grp12.softskilltools.Fragment;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,7 +12,10 @@ import android.widget.TextView;
 import com.galgespil.stvhendeop.menuapp.R;
 
 
+import com.grp12.softskilltools.Activities.MainMenu;
 import com.grp12.softskilltools.Entities.DISC;
+import com.grp12.softskilltools.Util.DISC_popup;
+import com.grp12.softskilltools.Util.PopUp;
 
 
 import org.eazegraph.lib.communication.IOnItemFocusChangedListener;
@@ -27,18 +31,15 @@ import java.text.DecimalFormat;
  * Created by mathiaslarsen on 27/11/2016.
  */
 
-public class DISCResultFragment extends Fragment {
+public class DISCResultFragment extends Fragment implements View.OnClickListener {
 
     private PieChart mPieChart;
     DISC temp;
     Bundle bundle;
-    TextView textDom;
-    TextView textInf;
-    TextView textSta;
-    TextView textCom;
+    TextView textDom, textInf, textSta, textCom,
+            popDom, popInf, popSta, popCom;
     public DecimalFormat df;
-
-
+    public int antal;
 
 
     public DISCResultFragment() {
@@ -57,6 +58,14 @@ public class DISCResultFragment extends Fragment {
         textInf = (TextView) view.findViewById(R.id.infText);
         textSta = (TextView) view.findViewById(R.id.staText);
         textCom = (TextView) view.findViewById(R.id.comText);
+        popDom = (TextView) view.findViewById(R.id.popDom);
+        popInf = (TextView) view.findViewById(R.id.popInf);
+        popSta = (TextView) view.findViewById(R.id.popSta);
+        popCom = (TextView) view.findViewById(R.id.popCom);
+        popDom.setOnClickListener(this);
+        popInf.setOnClickListener(this);
+        popSta.setOnClickListener(this);
+        popCom.setOnClickListener(this);
         bundle = getArguments();
         this.temp = (DISC) bundle.getSerializable("Item");
         df = new DecimalFormat("#.0");
@@ -95,5 +104,39 @@ public class DISCResultFragment extends Fragment {
 
     }
 
- }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.popDom:
+                Intent i = new Intent(this.getContext(), DISC_popup.class);
+                i.putExtra("antal",antal);
+                i.putExtra("overskrift",getResources().getString(R.string.dominantOverskrift));
+                i.putExtra("brødtekst",getResources().getString(R.string.dominantBrødtekst));
+                startActivity(i);
+                break;
+            case R.id.popInf:
+                i = new Intent(this.getContext(), DISC_popup.class);
+                i.putExtra("antal",antal);
+                i.putExtra("overskrift",getResources().getString(R.string.influentialOverskrift));
+                i.putExtra("brødtekst",getResources().getString(R.string.influentialBrødtekst));
+                startActivity(i);
+                break;
+            case R.id.popSta:
+                i = new Intent(this.getContext(), DISC_popup.class);
+                i.putExtra("antal",antal);
+                i.putExtra("overskrift",getResources().getString(R.string.stableOverskrift));
+                i.putExtra("brødtekst",getResources().getString(R.string.stableBrødtekst));
+                startActivity(i);
+                break;
+            case R.id.popCom:
+                i = new Intent(this.getContext(), DISC_popup.class);
+                i.putExtra("antal",antal);
+                i.putExtra("overskrift",getResources().getString(R.string.compliantOverskrift));
+                i.putExtra("brødtekst",getResources().getString(R.string.compliantBrødtekst));
+                startActivity(i);
+                break;
+        }
+    }
+
+}
 
