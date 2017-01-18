4286eee6aa33aa78e3a495d354a9b24c327f110a
rosenlund96 <Stumack3r>
2017-01-18 16:20:41 +0100

    Coachmark færdigt.

--- app/src/main/java/com/grp12/softskilltools/Activities/MainMenu.java
+++ app/src/main/java/com/grp12/softskilltools/Activities/MainMenu.java
@@ -72,8 +72,8 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
     private static MainMenu sMainMenu;
     public DatabaseReference mRootDataRef = FirebaseDatabase.getInstance().getReference();
     public DatabaseReference mConditionDataRef;
-    private static final int TOTAL_PAGES = 3;
-    private static final int ACTUAL_PAGES_COUNT = 3;
+    private static final int TOTAL_PAGES = 4;
+    private static final int ACTUAL_PAGES_COUNT = 4;
     public int[] mPagesColors;
     TextView nav_user;
     TextView nav_email;
@@ -551,6 +551,16 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
                             TransformItem.create(R.id.ivFifthImage, Direction.LEFT_TO_RIGHT, 0.03f),
                             TransformItem.create(R.id.ivSixthImage, Direction.LEFT_TO_RIGHT, 0.09f),
                             TransformItem.create(R.id.ivSeventhImage, Direction.LEFT_TO_RIGHT, 0.14f),
+                            TransformItem.create(R.id.ivEighthImage, Direction.LEFT_TO_RIGHT, 0.07f)
+                    };
+                    break;
+                }
+                case 3: {
+                    pageLayoutResId = R.layout.fragment_page_fourth;
+                    tutorialItems = new TransformItem[]{
+                            TransformItem.create(R.id.ivFirstImage, Direction.RIGHT_TO_LEFT, 0.2f),
+                            TransformItem.create(R.id.ivThirdImage, Direction.RIGHT_TO_LEFT, 0.08f),
+                            TransformItem.create(R.id.ivFourthImage, Direction.LEFT_TO_RIGHT, 0.1f),
                             TransformItem.create(R.id.ivEighthImage, Direction.LEFT_TO_RIGHT, 0.07f)
                     };
                     break;
