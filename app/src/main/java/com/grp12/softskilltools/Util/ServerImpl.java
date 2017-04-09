4cc054632ebb39536df4d8af798408cc2cbcbf4a
Mathias R. Larsen <mrl@cureit.dk>
2017-04-09 20:01:35 +0200

    Serveren er nu online og der kan i serverimpl klassen ses de forskellige funktioner på den, ved at tjekke linket i kommentaren i toppen.

---
+++ app/src/main/java/com/grp12/softskilltools/Util/ServerImpl.java
@@ -0,0 +1,20 @@
+package com.grp12.softskilltools.Util;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Mathias R. Larsen on 09-04-2017.
+ */
+
+/* SERVER TILGÅS VIA http://ubuntu4.javabog.dk:9959/softskills?WSDL*/
+
+public class ServerImpl extends AsyncTask<Object, Object, Void> {
+    @Override
+    protected Void doInBackground(Object... voids) {
+
+        return null;
+    }
+    }
+
+
+
